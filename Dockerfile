37baa6ae7f0e7d65494ef5c1a92261a9a231de83
Drake2202 <rafabrittosousa@gmail.com>
2022-08-02 21:32:34 -0300

    Updating files to upload into OCI

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ WORKDIR /var/www/html
 RUN docker-php-ext-install mysql mysqli
 
 EXPOSE 80
+EXPOSE 3306
--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$config['mysql']['host'] = "db";	// MySQL Server IP/Hostname
+$config['mysql']['host'] = "localhost";	// MySQL Server IP/Hostname
 $config['mysql']['user'] = "root";		// MySQL Username.
-$config['mysql']['pass'] = "$Rafa1234";		// MySQL Password.
+$config['mysql']['pass'] = '$Rafa1234';		// MySQL Password.
 $config['mysql']['name'] = "meh"; 		// MySQL Database name.
 
 mysql_connect($config['mysql']['host'], $config['mysql']['user'], $config['mysql']['pass']) or die(mysql_error());
